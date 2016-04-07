d4ae5fe309e0aeea0455b611bc8fe86671496a57
CodeBlastr <github@razorit.com>
2016-04-07 19:02:18 -0400

    wholesale360.com removed bootstrap because manually loading is required for dependencies on this plugin

--- config/bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-// these need to be in the App/config/bootstrap.php file (sorry)
-//use Cake\Core\Plugin;
-//use Cake\Core\Configure;
-//
-//Plugin::load('Bootstrap');
-//Plugin::load('Less');
-//Configure::write('Auth.flash.params.class', ['alert', 'alert-danger', 'alert-dismissible']); // odd fix to get bootstrapui, and cakedc users to play nice together
-//Plugin::load('BootstrapUI');
